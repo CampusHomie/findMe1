9b8d19794470d08267febcc88ee07ad1b12469ce
okori-d <71137081+okori-d@users.noreply.github.com>
2022-01-30 19:47:57 +0300

    Update directions_repository.dart

--- flutter_gmaps-main/flutter_gmaps-main/lib/directions_repository.dart
+++ flutter_gmaps-main/flutter_gmaps-main/lib/directions_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_gmaps/.env.dart';
 import 'package:flutter_gmaps/directions_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,7 +10,7 @@ class DirectionsRepository {
 
   final Dio _dio;
   
-  String googleAPIKey = "AIzaSyBeOyY9O2d-W3GzzGcEiUJf1dD5GqJRfjs";
+  //String googleAPIKey = "AIzaSyBeOyY9O2d-W3GzzGcEiUJf1dD5GqJRfjs";
 
   DirectionsRepository({Dio dio}) : _dio = dio ?? Dio();
 
